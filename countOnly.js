f7f738e7fc15711794c8a7511af7099c59b62a71
Russel Mueller <bohjaangles@gmail.com>
2022-10-19 23:51:47 +0000

    added a program to count all instances in an inputted array as indicated by inputted object

---
+++ countOnly.js
@@ -0,0 +1,48 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`🚒🚒🚒Assertion Failed = ${actual} !== ${expected}`);
+    return;
+  }
+
+  console.log(`🚛🚛🚛Assertion passed = ${actual} === ${expected}`);
+
+};
+
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = function(allItems, itemsToCount) {
+  let result = {};
+  for (let item of allItems){
+      if (itemsToCount[item]){
+        if (result[item]){
+          result[item] += 1;
+        } else {
+          result[item] = 1;
+        } 
+      }
+    console.log('item', item);
+  }
+  console.log('result', result);
+  return result;
+}
+
+
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
