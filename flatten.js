dcbb47d6e79548a66c5bcef6bf3d90aa405014ec
Russel Mueller <bohjaangles@gmail.com>
2022-10-19 04:55:39 +0000

    made a func to bring arrays within arrays to the same level as a new arr

---
+++ flatten.js
@@ -0,0 +1,17 @@
+const flatten = function(input) {
+  let result = [];
+  // let result = mt array
+  //fouble for loop, second one is checking if there is a nested loop, if not then move that to result
+  for (let i = 0; i < input.length; i++) {
+    if (typeof(input[i]) != 'object') {
+      result.push(input[i]);
+    } else {
+      for (let j = 0; j < input[i].length; j++) {
+        result.push(input[i][j]);
+      }
+    }
+  }
+  return result;
+};
+
+console.log(flatten([1, 2, [3, 4], 5, [6]])); // => [1, 2, 3, 4, 5, 6]
