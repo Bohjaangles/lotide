be5732e2363b868480821d9e64fde12e0d232eae
Russel Mueller <bohjaangles@gmail.com>
2022-10-24 16:50:52 +0000

    refactored eqArrays to export its function and made a eqArraysTest in the test subdirectory and moved the eqArrays tests over to that, confirmed it all works as expected

--- eqArrays.js
+++ eqArrays.js
@@ -1,13 +1,4 @@
 
-const assertEqual = function(actual, expected) {
-  if (actual !== expected) {
-    console.log(`🚒🚒🚒Assertion Failed = ${actual} !== ${expected}`);
-    return;
-  } else {
-    console.log(`🚛🚛🚛Assertion passed = ${actual} === ${expected}`);
-  }
-};
-
 const eqArrays = function(actual, expected) {
   if (actual.length !== expected.length) {
     return false;
@@ -22,11 +13,4 @@ const eqArrays = function(actual, expected) {
   return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 3]), false); // => should PASS
-
-eqArrays([1, 2, 3], [1, 2, 3]); // => true
-eqArrays([1, 2, 3], [3, 2, 1]); // => false
-
-eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
+module.exports = eqArrays;
